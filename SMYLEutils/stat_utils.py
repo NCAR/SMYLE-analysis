665b7e4facc847711cdb2cdc84cceee9836127c0
Stephen Yeager <yeager@ucar.edu>
2021-08-31 21:03:10 -0600

    Added function to compute and remove leadtime-dependent drift

--- SMYLEutils/stat_utils.py
+++ SMYLEutils/stat_utils.py
@@ -1,6 +1,7 @@
 import xarray as xr
 import numpy as np
 import sys
+import cftime
 
 
 def cor_ci_bootyears(ts1, ts2, seed=None, nboots=1000, conf=95):
@@ -31,3 +32,26 @@ def cor_ci_bootyears(ts1, ts2, seed=None, nboots=1000, conf=95):
 
     return minci, maxci 
 
+
+def remove_drift(da, da_time, y1, y2):
+    """
+    Function to convert raw DP DataArray into anomaly DP DataArray with leadtime-dependent climatology removed.
+    --Inputs--
+        da:  Raw DP DataArray with dimensions (Y,L,M,...)
+        da_time:  Verification time of DP DataArray (Y,L)
+        y1:  Start year of climatology
+        y2:  End year of climatology
+        
+    --Outputs--
+        da_anom:  De-drifted DP DataArray
+        da_climo:  Leadtime-dependent climatology
+    
+    Author: E. Maroon (modified by S. Yeager)
+    """
+    d1 = cftime.DatetimeNoLeap(y1,1,1,0,0,0)
+    d2 = cftime.DatetimeNoLeap(y2,12,31,23,59,59)
+    masked_period = da.where((da_time>d1) & (da_time<d2))
+    da_climo = masked_period.mean('M').mean('Y')
+    da_anom = da - da_climo
+
+    return da_anom, da_climo
