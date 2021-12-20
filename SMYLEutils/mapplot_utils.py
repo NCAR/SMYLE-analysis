9075eb0c179e0bfacdb30a0ec44dc6b10bb49705
Kristen Krumhardt <kristen.krumhardt@colorado.edu>
2021-12-20 13:12:40 -0700

    bgc processing nbs abd figure

--- SMYLEutils/mapplot_utils.py
+++ SMYLEutils/mapplot_utils.py
@@ -116,3 +116,33 @@ def map_contourf_global_subplot(fig, dat, lon, lat, ci, cmin, cmax, titlestr, le
     return ax, cntr
 
 
+def adjust_pop_grid(tlon,tlat,field):
+    nj = tlon.shape[0]
+    ni = tlon.shape[1]
+    xL = int(ni/2 - 1)
+    xR = int(xL + ni)
+
+    tlon = np.where(np.greater_equal(tlon,min(tlon[:,0])),tlon-360.,tlon)
+    lon  = np.concatenate((tlon,tlon+360.),1)
+    lon = lon[:,xL:xR]
+
+    if ni == 320:
+        lon[367:-3,0] = lon[367:-3,0]+360.
+    lon = lon - 360.
+    lon = np.hstack((lon,lon[:,0:1]+360.))
+    if ni == 320:
+        lon[367:,-1] = lon[367:,-1] - 360.
+
+    #-- trick cartopy into doing the right thing:
+    #   it gets confused when the cyclic coords are identical
+    lon[:,0] = lon[:,0]-1e-8
+    
+    #-- periodicity
+    lat  = np.concatenate((tlat,tlat),1)
+    lat = lat[:,xL:xR]
+    lat = np.hstack((lat,lat[:,0:1]))
+
+    field = np.ma.concatenate((field,field),1)
+    field = field[:,xL:xR]
+    field = np.ma.hstack((field,field[:,0:1]))
+    return lon,lat,field
